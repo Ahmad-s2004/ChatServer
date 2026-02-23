572c8d145f1671c728a49066be234c5c147a8d96
Ahmad Amman <132729640+Ahmad-s2004@users.noreply.github.com>
2026-02-23 21:53:51 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,21 +1,5 @@
-import express from "express"
-const app = express()
-import dotenv from 'dotenv'
-import dbconnection from "./config/db.js"
-import authRouter from "./routers/authRouter.js" 
+import Message from "./Message.model.js"
+import User from "./User.model.js"
+import Chat from "./Chat.model.js"
 
-dotenv.config()
-dbconnection()
-
-app.use(express.json())
-app.use("/auth", authRouter)
-
-
-
-const PORT = process.env.PORT || 4550
-app.listen(PORT, ()=>{
-    console.log(`Server is started at port ${PORT}`)
-})
-
-
-
+export { User, Chat, Message };
