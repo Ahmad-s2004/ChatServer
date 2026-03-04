72e72a5fcaaa911ae3b797a81d279d755b9728f1
Ahmad-s2004 <shahidahmadamman@gmail.com>
2026-03-04 23:17:26 +0500

    auth.js updated

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,1 +1,20 @@
-import {User} from "../models/User.model.js"
+import jwt from "jsonwebtoken"
+import User from "../models/userModel.js"
+
+const auth = async (req, res, next) => {
+    try {
+        const token = req.cookies?.token
+
+        if (!token) return res.status(401).json({ message: "Login required" })
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+        req.user = await User.findById(decoded.id).select("-password")
+
+        next()
+    } catch (err) {
+        res.status(401).json({ message: "Unauthorized" })
+    }
+}
+
+export default auth
