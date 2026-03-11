6eac51807abba3c88094948f3b6927862d66a1dd
Ahmad-s2004 <shahidahmadamman@gmail.com>
2026-03-11 22:44:16 +0500

    create getSingleUser controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -15,11 +15,33 @@ const getAllUsers = async (req, res) => {
   }
 }
 
-
+const getSingleUser = async (req, res) => {
+    try {
+  
+      const { id } = req.params
+  
+      const user = await User.findById(id)
+  
+      if (!user) {
+        return res.status(404).json({ message: "User not found" })
+      }
+  
+      res.status(200).json(user)
+  
+    } catch (error) {
+  
+      res.status(500).json({ message: "Server Error" })
+  
+    }
+  }
+  
+  
+  
 
 
 
 
 module.exports = {
-  getAllUsers
+  getAllUsers,
+  getSingleUser
 }
