e7cad2daa1e5c3fcbfbbdf7b4923b280da0cb148
Ahmad-s2004 <shahidahmadamman@gmail.com>
2026-03-17 18:59:13 +0500

    modify userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,71 +1,45 @@
 const User = require("../models/index.js")
 
 
-const getAllUsers = async (req, res) => {
+const getAllUser = async (req, res) => {
   try {
-
-    const users = await User.find({ _id: { $ne: req.user._id } })
-
+    const users = await User.find({ _id: req.user._id})
     res.status(200).json(users)
-
   } catch (error) {
-
     res.status(500).json({ message: "Server Error" })
-
   }
 }
 
 const getSingleUser = async (req, res) => {
     try {
-  
       const { id } = req.params
-  
       const user = await User.findById(id)
-  
       if (!user) {
         return res.status(404).json({ message: "User not found" })
       }
-  
       res.status(200).json(user)
-  
     } catch (error) {
-  
       res.status(500).json({ message: "Server Error" })
-  
     }
   }
   
   const searchUser = async (req, res) => {
     try {
-  
       const keyword = req.query.search
-        ? {
-            $or: [
+        ? {$or: [
               { name: { $regex: req.query.search, $options: "i" } },
-              { email: { $regex: req.query.search, $options: "i" } }
-            ]
+              { email: { $regex: req.query.search, $options: "i" } }]
           }
         : {}
-  
-      const users = await User.find(keyword).find({
-        _id: { $ne: req.user._id }
-      })
-  
+      const users = await User.find(keyword).find({_id: req.user._id})
       res.status(200).json(users)
-  
     } catch (error) {
-  
       res.status(500).json({ message: "Server Error" })
-  
     }
   }
-  
 
-
-
-
-module.exports = {
-  getAllUsers,
+exports = {
+  getAllUser,
   getSingleUser,
   searchUser
 }
