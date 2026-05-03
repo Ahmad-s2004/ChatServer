6a7398f21aa6611d8b26487b4d7fbbcc4088d74c
Ahmad-s2004 <shahidahmadamman@gmail.com>
2026-05-03 13:22:26 +0500

    fix: resolve cross-origin issues by implementing cors middleware in app.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import userRouter from "./routers/userRoute.js";
 import messageRouter from "./routers/messageRoute.js";
 import cookieParser from "cookie-parser";
 import { init } from "./utils/socket.js";
+import cors from "cors"
 
 dotenv.config();
 dbconnection();
@@ -17,6 +18,11 @@ const io = init(server);
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true,
+    methods:['POST','GET','UPDATE','DELETE'],
+}))
 app.use("/auth", authRouter);
 app.use("/user", userRouter);
 app.use("/message", messageRouter);
